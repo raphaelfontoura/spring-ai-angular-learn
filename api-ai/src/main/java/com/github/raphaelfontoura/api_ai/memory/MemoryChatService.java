452b146f9b85b06edd4d30ab57046a6fe0cda623
raphael fontoura <raphael.fontoura@serpro.gov.br>
2025-10-08 00:26:16 -0300

    feat: add MySQL configuration and schema for chat memory functionality

--- api-ai/src/main/java/com/github/raphaelfontoura/api_ai/memory/MemoryChatService.java
+++ api-ai/src/main/java/com/github/raphaelfontoura/api_ai/memory/MemoryChatService.java
@@ -2,7 +2,10 @@ package com.github.raphaelfontoura.api_ai.memory;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,16 +13,22 @@ public class MemoryChatService {
 
     private final ChatClient chatClient;
 
-    public MemoryChatService(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public MemoryChatService(ChatClient.Builder chatClientBuilder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(10)
+                .build();
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        new SimpleLoggerAdvisor()
                 )
                 .build();
     }
 
     public String simpleChat(String message) {
         return this.chatClient.prompt()
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "user-123"))
                 .user(message)
                 .call()
                 .content();
